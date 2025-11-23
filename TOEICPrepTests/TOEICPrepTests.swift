4d050a2a1e34c90f0d54ddffd56e801aa15096ab
jeffchoux <jeffchoux@hotmail.com>
2025-11-23 19:07:35 +0400

    Initial Commit

---
+++ TOEICPrepTests/TOEICPrepTests.swift
@@ -0,0 +1,16 @@
+//
+//  TOEICPrepTests.swift
+//  TOEICPrepTests
+//
+//  Created by JF CHOUX on 23/11/2025.
+//
+
+import Testing
+
+struct TOEICPrepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
